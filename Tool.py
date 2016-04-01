4be992bd7e7b8f040eb72ac71870ad81a46815b7
Frichetten <frichetten@gmail.com>
2016-04-01 18:07:52 -0500

    First Chart

--- Tool.py
+++ Tool.py
@@ -1,10 +1,11 @@
 import csv
 from collections import Counter
+from matplotlib import pyplot as plt
 
-def fileIO():
+def fileIO(filename):
     #Attempt to open file
     try:
-        file = open('Qtr01_Circum_Person.csv', 'r+')
+        file = open(filename, 'r+')
     except IOError:
         inp = raw_input("Cannot find file. Create one? (y/n)")
         if inp == 'y':
@@ -14,9 +15,40 @@ def fileIO():
             print "Ending."
     return file
 
-reader = csv.reader(fileIO())
-data = []
+filename = 'Qtr01_Accident.csv'
+reader = csv.reader(fileIO(filename))
+Q1counter = -1
 for row in reader:
-       data.append(row[2])
+       Q1counter += 1
 
-print Counter(data).most_common()
+filename = 'Qtr02_Accident.csv'
+reader= csv.reader(fileIO(filename))
+Q2counter = -1
+for row in reader:
+       Q2counter += 1
+
+filename = 'Qtr03_Accident.csv'
+reader= csv.reader(fileIO(filename))
+Q3counter = -1
+for row in reader:
+       Q3counter += 1
+
+filename = 'Qtr04_Accident.csv'
+reader= csv.reader(fileIO(filename))
+Q4counter = -1
+for row in reader:
+       Q4counter += 1
+
+sum = Q1counter + Q2counter + Q3counter + Q4counter
+
+accidents = ["Q1 Accidents", "Q2 Accidents", "Q3 Accidents", "Q4 Accidents"]
+sums = [Q1counter, Q2counter, Q3counter, Q4counter]
+xs = [i+0.1 for i, _ in enumerate(accidents)]
+plt.bar(xs,sums)
+
+plt.ylabel("Number of Accidents")
+plt.title("Number of Accidents Per Quarter")
+
+plt.xticks([i + 0.5 for i, _ in enumerate(accidents)], accidents)
+
+plt.show()
